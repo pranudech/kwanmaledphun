3de4140e5b6f3f73a7cc723ea406355f48ccb6e4
Pranudech <pranudech@gmail.com>
2024-08-17 02:26:35 +0700

    update api

--- src/services/CategoryServices.js
+++ src/services/CategoryServices.js
@@ -1,13 +1,13 @@
 import requests from "./httpServices";
 import show from "./json/category/show.json";
 
-const BASE_URL = "http://localhost:3010"
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL_ENDPOINT;
 
 const CategoryServices = {
   getShowingCategory: async () => {
     // return requests.get("/category/show");
     // return show;
-    return requests.get(`${BASE_URL}/api/product_type`);
+    return requests.get(`${BASE_URL}/product_type`);
   },
 };
 
