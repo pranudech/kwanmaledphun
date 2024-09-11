cfd5cc76dc832c5416cbc62da94e65c0768f2260
Pranudech <pranudech@gmail.com>
2024-09-11 16:29:53 +0700

    fix server.js

---
+++ app.js
@@ -0,0 +1,10 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = 'It works!\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen(3001);
