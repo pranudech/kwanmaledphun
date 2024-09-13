919fb71aa0b557fc0b8e648d11b8953bd41482a8
Pranudech <pranudech@gmail.com>
2024-09-13 18:06:13 +0700

    update

--- src/lib/tidb.js
+++ src/lib/tidb.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2')
-// export const mysqlPool = mysql.createPool(process.env.MYSQL_URI)
-export const mysqlPool = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'kwanmxwf_dev',
-    password: '7?*VRnEAUU^B',
-    database: 'kwanmxwf_dev',
-})
+export const mysqlPool = mysql.createPool(process.env.MYSQL_URI)
+// export const mysqlPool = mysql.createConnection({
+//     host: process.env.DB_HOST || 'localhost',
+//     port: process.env.DB_PORT || 3306,
+//     user: process.env.DB_USER || 'kwanmxwf_dev',
+//     password: process.env.DB_PASSWORD || '7?*VRnEAUU^B',
+//     database: process.env.DB_NAME || 'kwanmxwf_dev',
+// })
